d41856cb0664b4bf78da764e9d77a29f9f864160
KOITAN_DESKTOP\tk_ka <tk_kakutasu@yahoo.co.jp>
2023-08-19 17:38:45 +0900

    先輩の動きを仮実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,119 +7,72 @@ using DG.Tweening;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject playerSheep;
-    bool isJumping = false;
-    float bpm = 120f;
-    float beatSeconds;
+    Sheep playerSheepPrefab;
+    [SerializeField]
+    Transform playerSheepTf;
+    public float Bpm { get; private set; } = 120f;
+    public float BeatSeconds { get; private set; }
+    public bool IsGameOver { get; private set; }
     int cnt;
+    [SerializeField]
+    MusicalScore musicalScore;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        beatSeconds = 60f / bpm;
-        //playerSheep.transform.DOMoveX(16f, beatSeconds * 4f).SetEase(Ease.Linear).SetRelative().SetLoops(-1, LoopType.Restart);
+        Time.timeScale = 1f;
+        BeatSeconds = 60f / Bpm;
+    }
 
-        Sequence seq = DOTween.Sequence()
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
-            })
-            .AppendInterval(beatSeconds)
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
-            })
-            .AppendInterval(beatSeconds)
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
-            })
-            .AppendInterval(beatSeconds)
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
-            })
-            .AppendInterval(beatSeconds)
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
-            })
-            .AppendInterval(beatSeconds)
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
-            })
-            .AppendInterval(beatSeconds)
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
-            })
-            .AppendInterval(beatSeconds)
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
-            })
-            .AppendInterval(beatSeconds)
-            .AppendCallback(() =>
-            {
-                playerSheep.transform.DOKill();
-                playerSheep.transform.position = new Vector3(-16f, 0f);
-            })
-            .SetLoops(-1, LoopType.Restart);
-
+    private void Start()
+    {
+        StartLoop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
-        {
-            Sequence seq = DOTween.Sequence()
-                .AppendCallback(() =>
-                {
-                    isJumping = true;
-                    playerSheep.transform.DOMoveY(3f, 0.125f).SetRelative().SetEase(Ease.OutExpo).SetLoops(2, LoopType.Yoyo);
-                })
-                .AppendInterval(0.25f)
-                .AppendCallback(() =>
-                {
-                    isJumping = false;
-                })
-                ;
-        }
         KoitanDebug.Display($"デバッグ表示です\n");
         KoitanDebug.Display($"Time.time = {Time.time}\n");
     }
 
-    /*
-    void MoveOneStep()
+    void StartLoop()
     {
         Sequence seq = DOTween.Sequence()
             .AppendCallback(() =>
             {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
-                playerSheep.transform.DOMoveX(2f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+                InstantiateSheep(cnt % 2 == 0);
+                cnt++;
             })
-            .AppendInterval(beatSeconds)
+            .AppendInterval(BeatSeconds * 4f)
             .AppendCallback(() =>
             {
-                cnt++;
-                if (cnt == 4)
+                if (!IsGameOver)
                 {
-                    cnt = 0;
-                    playerSheep.transform.position = Vector3.zero;
-                    Debug.Log($"cnt = {cnt}");
+                    StartLoop();
                 }
-                MoveOneStep();
             })
             ;
     }
-    */
+
+    void InstantiateSheep(bool isTutorial)
+    {
+        Sheep sheep = Instantiate(playerSheepPrefab, playerSheepTf.position, playerSheepTf.rotation);
+        sheep.gameObject.SetActive(true);
+        sheep.gameManager = this;
+        sheep.musicalScore = musicalScore;
+        sheep.isTutorial = isTutorial;
+    }
+}
+
+[System.Serializable]
+public class MusicalScore
+{
+    public NoteType[] notes;
+}
+
+public enum NoteType
+{
+    None,
+    Single,
+    Double,
 }
---
+++ Assets/Scripts/Sheep.cs
@@ -0,0 +1,159 @@
+using DG.Tweening;
+using KoitanLib;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public GameManager gameManager;
+    bool isJumping = false;
+    TrailRenderer trailRenderer;
+    public bool isTutorial;
+    public MusicalScore musicalScore;
+    Sequence scaleSeq;
+
+    Vector3 position;
+    Vector3 velocity;
+    [SerializeField]
+    Vector3 acc;
+    [SerializeField]
+    Vector3 jumpSpeed;
+    bool isGround = true;
+    int airJumpCount = 1;
+    int airJumpCountMax = 1;
+
+    void Start()
+    {
+        scaleSeq = DOTween.Sequence()
+            .AppendCallback(() =>
+            {
+                transform.DOPunchScale(Vector3.one * 0.1f, 0.1f);
+            })
+            .AppendInterval(gameManager.BeatSeconds)
+            .SetLoops(-1, LoopType.Restart)
+            ;
+        Sequence seq = DOTween.Sequence()
+            .AppendCallback(() =>
+            {
+                transform.DOMoveX(32f, gameManager.BeatSeconds * 8f).SetEase(Ease.Linear).SetRelative();
+            })
+            .AppendInterval(gameManager.BeatSeconds * 8f)
+            .AppendCallback(() =>
+            {
+                // 破棄
+                transform.DOKill();
+                scaleSeq.Kill();
+                Destroy(gameObject);
+            })
+            ;
+        if (isTutorial)
+        {
+            // ジャンプ
+            Sequence jumpSeq = DOTween.Sequence()
+            .AppendInterval(gameManager.BeatSeconds * 2f)
+            .AppendCallback(() =>
+            {
+                if (musicalScore.notes[0] == NoteType.Single)
+                {
+                    Jump();
+                }
+            })
+            .AppendInterval(gameManager.BeatSeconds)
+            .AppendCallback(() =>
+            {
+                if (musicalScore.notes[1] == NoteType.Single)
+                {
+                    Jump();
+                }
+            })
+            .AppendInterval(gameManager.BeatSeconds)
+            .AppendCallback(() =>
+            {
+                if (musicalScore.notes[2] == NoteType.Single)
+                {
+                    Jump();
+                }
+            })
+            .AppendInterval(gameManager.BeatSeconds)
+            .AppendCallback(() =>
+            {
+                if (musicalScore.notes[3] == NoteType.Single)
+                {
+                    Jump();
+                }
+            })
+            ;
+
+        }
+    }
+
+    void Update()
+    {
+        // 座標計算
+        position = transform.position;
+        velocity += acc * Time.deltaTime;
+        position += velocity * Time.deltaTime;
+        // 地面に着地
+        if (position.y < 0)
+        {
+            airJumpCount = airJumpCountMax;
+            position.y = 0;
+            isGround = true;
+        }
+        transform.position = position;
+
+        //transform.position += Vector3.up * 1 * Time.deltaTime;
+        if (isTutorial)
+        {
+
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (isGround)
+                {
+                    Jump();
+                }
+                else
+                {
+                    if (airJumpCount > 0)
+                    {
+                        airJumpCount--;
+                        Jump();
+                    }
+                }
+            }
+        }
+        /*
+        KoitanDebug.Display($"デバッグ表示です\n");
+        KoitanDebug.Display($"Time.time = {Time.time}\n");
+        */
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("GameOver");
+    }
+
+    void Jump()
+    {
+        isGround = false;
+        velocity = jumpSpeed;
+        /*
+        Sequence seq = DOTween.Sequence()
+                    .AppendCallback(() =>
+                    {
+                        isJumping = true;
+                        transform.DOMoveY(2f, 0.2f).SetRelative().SetEase(Ease.OutCubic).SetLoops(2, LoopType.Yoyo);
+                    })
+                    .AppendInterval(0.4f)
+                    .AppendCallback(() =>
+                    {
+                        isJumping = false;
+                    })
+                    ;
+        */
+    }
+}
