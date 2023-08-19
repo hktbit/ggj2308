139ef788631a8381146437dc651876825fd17e2c
KOITAN_DESKTOP\tk_ka <tk_kakutasu@yahoo.co.jp>
2023-08-19 14:27:00 +0900

    文字コードの変更

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using KoitanLib;
@@ -11,19 +11,69 @@ public class GameManager : MonoBehaviour
     bool isJumping = false;
     float bpm = 120f;
     float beatSeconds;
+    int cnt;
     // Start is called before the first frame update
     void Start()
     {
         beatSeconds = 60f / bpm;
-        playerSheep.transform.DOMoveX(16f, beatSeconds * 4f).SetEase(Ease.Linear).SetRelative().SetLoops(-1, LoopType.Restart);
+        //playerSheep.transform.DOMoveX(16f, beatSeconds * 4f).SetEase(Ease.Linear).SetRelative().SetLoops(-1, LoopType.Restart);
+
         Sequence seq = DOTween.Sequence()
             .AppendCallback(() =>
             {
-                playerSheep.transform.DOPunchScale(Vector3.one * 0.1f, 0.1f);
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
             })
             .AppendInterval(beatSeconds)
-            .SetLoops(-1, LoopType.Restart)
-            ;
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+            })
+            .AppendInterval(beatSeconds)
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+            })
+            .AppendInterval(beatSeconds)
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+            })
+            .AppendInterval(beatSeconds)
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+            })
+            .AppendInterval(beatSeconds)
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+            })
+            .AppendInterval(beatSeconds)
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+            })
+            .AppendInterval(beatSeconds)
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(4f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+            })
+            .AppendInterval(beatSeconds)
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOKill();
+                playerSheep.transform.position = new Vector3(-16f, 0f);
+            })
+            .SetLoops(-1, LoopType.Restart);
+
     }
 
     // Update is called once per frame
@@ -35,16 +85,41 @@ public class GameManager : MonoBehaviour
                 .AppendCallback(() =>
                 {
                     isJumping = true;
-                    playerSheep.transform.DOMoveY(3f, 0.15f).SetRelative().SetEase(Ease.OutExpo).SetLoops(2, LoopType.Yoyo);
+                    playerSheep.transform.DOMoveY(3f, 0.125f).SetRelative().SetEase(Ease.OutExpo).SetLoops(2, LoopType.Yoyo);
                 })
-                .AppendInterval(0.3f)
+                .AppendInterval(0.25f)
                 .AppendCallback(() =>
                 {
                     isJumping = false;
                 })
                 ;
         }
-        KoitanDebug.Display($"ƒfƒoƒbƒO•\Ž¦‚Å‚·\n");
+        KoitanDebug.Display($"ãƒ‡ãƒãƒƒã‚°è¡¨ç¤ºã§ã™\n");
         KoitanDebug.Display($"Time.time = {Time.time}\n");
     }
+
+    /*
+    void MoveOneStep()
+    {
+        Sequence seq = DOTween.Sequence()
+            .AppendCallback(() =>
+            {
+                playerSheep.transform.DOPunchScale(Vector3.one * 0.2f, 0.1f);
+                playerSheep.transform.DOMoveX(2f, beatSeconds).SetEase(Ease.OutExpo).SetRelative();
+            })
+            .AppendInterval(beatSeconds)
+            .AppendCallback(() =>
+            {
+                cnt++;
+                if (cnt == 4)
+                {
+                    cnt = 0;
+                    playerSheep.transform.position = Vector3.zero;
+                    Debug.Log($"cnt = {cnt}");
+                }
+                MoveOneStep();
+            })
+            ;
+    }
+    */
 }
